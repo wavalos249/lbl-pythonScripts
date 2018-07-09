8c0c605e7e7f01f066758eeae5d9134b6efd3bd4
Wendy Avalos <wwavalos@hotmail.com>
2018-07-09 12:22:42 -0700

    adding new version of script and requirements file

--- passwords.py
+++ passwords.py
@@ -1,3 +1,4 @@
+import ldap
 import os
 import sys
 
@@ -76,5 +77,37 @@ def main():
     print ('read contents from "%s" and "%s". wrote results to "%s"' % (lrc_filepath, ldap_filepath, output_filepath))
 
 
+
+def found_in_ldap( user ):
+    #print ldap.__file__
+    con = ldap.initialize('ldap://identity:389')
+    ldap_base = "ou=people,dc=lbl,dc=gov"
+    #searchAttribute = ["uid","cn","mail"]
+    searchAttribute = ["mail"]
+    #query = "(uid=wavalos)"
+    query = "(uid=%s)" % user		# uid is loginname eg "wavalos"
+    #query = "(sn=%s)" % user		# sn is "surname"
+    #query = "(givenname=%s)" % user
+    #result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query)    # unfiltered results
+    result_set = con.search_s(ldap_base, ldap.SCOPE_SUBTREE, query, searchAttribute)
+    print( result_set )
+    print( len(result_set) )	# number of matches
+    #return result_set		# could return full result for caller to handle if desired
+    return len(result_set)
+# END found_in_ldap( user )
+
+
+def test_ldap():
+    found_in_ldap("wendy")
+    result_set = found_in_ldap("tin")
+    print( result_set )
+    print( "===" )
+    found_in_ldap("foobar")
+    if found_in_ldap("wavalos") :
+       print( "found wavalos" )
+    return 0
+
+
 if __name__ == '__main__':
-    main()
+    #main()
+    test_ldap()
